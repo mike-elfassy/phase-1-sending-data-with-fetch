0c842acbe2d90350b5a65bcaf2dde990479d5e85
Michael Elfassy <mike.elfassy@gmail.com>
2022-12-13 23:55:06 -0500

    commit tree ff809cfc57902c069e7150d437b2f6c274330e09

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return (
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name: userName,
+                email: userEmail
+            })
+        })
+        .then(response => response.json())
+        .then(object => addStringToDom(object.id))
+        .catch(error => addStringToDom(error.message))
+    )
+}
+
+function addStringToDom(string) {
+    let item = document.createElement('p')
+    item.textContent = string
+    document.querySelector('body').appendChild(item)
+}
